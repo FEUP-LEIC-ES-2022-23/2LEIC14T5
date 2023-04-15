5810256f66865e07755f99c2987f3fd80ec58602
João Silva <joaosilva190803@gmail.com>
2023-04-15 20:15:45 +0100

    Fixed filters bug

--- app/lib/itjobs_api/itjobs_api.dart
+++ app/lib/itjobs_api/itjobs_api.dart
@@ -13,11 +13,108 @@ class ITJobsAPI {
       }
 
       else if(key == 'jobType' && body[key] != 'Unspecified'){
-        finalBody['type'] = body[key].toString();
+        switch(body[key]){
+          case 'Full-Time':
+            finalBody['job_type'] = '1';
+            break;
+          case 'Part-Time':
+            finalBody['job_type'] = '2';
+            break;
+          default:
+            break;
+        }
       }
 
       else if(key == 'contractType' && body[key] != 'Unspecified'){
-        finalBody['contract'] = body[key].toString();
+        switch(body[key]){
+          case 'Estágio Académico':
+            finalBody['contract_type'] = '5';
+            break;
+          case 'Prestação de Serviços':
+            finalBody['contract_type'] = '4';
+            break;
+          case 'Estágio Profissional':
+            finalBody['contract_type'] = '3';
+            break;
+          case 'Contrato sem termo':
+            finalBody['contract_type'] = '2';
+            break;
+          case 'Contrato a termo':
+            finalBody['contract_type'] = '1';
+            break;
+          default:
+            break;
+        }
+      }
+
+      else if(key == 'location' && body[key] != "Unspecified"){
+        switch(body[key]){
+          case 'Aveiro':
+            finalBody['location'] = '1';
+            break;
+          case 'Beja':
+            finalBody['location'] = '3';
+            break;
+          case 'Braga':
+            finalBody['location'] = '4';
+            break;
+          case 'Bragança':
+            finalBody['location'] = '5';
+            break;
+          case 'Castelo Branco':
+            finalBody['location'] = '6';
+            break;
+          case 'Coimbra':
+            finalBody['location'] = '8';
+            break;
+          case 'Évora':
+            finalBody['location'] = '10';
+            break;
+          case 'Faro':
+            finalBody['location'] = '9';
+            break;
+          case 'Guarda':
+            finalBody['location'] = '11';
+            break;
+          case 'Leiria':
+            finalBody['location'] = '13';
+            break;
+          case 'Lisboa':
+            finalBody['location'] = '14';
+            break;
+          case 'Portalegre':
+            finalBody['location'] = '12';
+            break;
+          case 'Porto':
+            finalBody['location'] = '18';
+            break;
+          case 'Santarém':
+            finalBody['location'] = '20';
+            break;
+          case 'Setúbal':
+            finalBody['location'] = '17';
+            break;
+          case 'Viana do Castelo':
+            finalBody['location'] = '22';
+            break;
+          case 'Vila Real':
+            finalBody['location'] = '21';
+            break;
+          case 'Viseu':
+            finalBody['location'] = '16';
+            break;
+          case 'Açores':
+            finalBody['location'] = '2';
+            break;
+          case 'Madeira':
+            finalBody['location'] = '15';
+            break;
+          case 'Internacional':
+            finalBody['location'] = '29';
+            break;
+          default:
+            break;
+        }
       }
 
       else if(body[key] != 'Unspecified' && key != 'limit'){
@@ -33,6 +130,7 @@ class ITJobsAPI {
     if(query.isNotEmpty){
       finalBody['q'] = query;
     }
+
     finalBody['limit'] = body['limit'];
 
     final response = await http.post(Uri.parse('https://api.itjobs.pt/job/list.json'), body: finalBody);
