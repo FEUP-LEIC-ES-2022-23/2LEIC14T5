6737f09ae9bc3f191f2dd55b987b6df0ac162ee8
João Longras <joaolongras2003@gmail.com>
2023-04-24 19:20:55 +0100

    Done Reset password

--- app/lib/authentication/forgot_password_page.dart
+++ app/lib/authentication/forgot_password_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
@@ -8,11 +10,78 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+  @override
+  void dispose(){
+    _emailController.dispose();
+    super.dispose();
+  }
+  Future passwordReset() async{
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(context: context, builder: (context){
+        return  AlertDialog(
+          backgroundColor: Colors.deepOrange,
+          content: Text('Password reset link sent! Check your email'),
+        );
+      });
+    } on FirebaseAuthException catch(e){
+      showDialog(context: context, builder: (context){
+        return AlertDialog(
+          backgroundColor: Colors.deepOrange,
+          content: Text(e.message.toString()),
+
+        );
+      });
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        body: Text('Enter Your email and we will send you a password '
-            'reset link'),
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          elevation: 0,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text('Enter Your email and we will send you a password '
+                  'reset link',textAlign: TextAlign.center,style: TextStyle(fontSize: 20),),
+            ),
+            const SizedBox(height: 10),
+            //email textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.orangeAccent),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: 'Email',
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            MaterialButton(onPressed: passwordReset,
+              color: Colors.deepOrange,
+              child: const Text('Reset Password'),
+            )
+          ],
+          //email text field
+
+        ),
     );
   }
 }
